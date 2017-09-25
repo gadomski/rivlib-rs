12a16252d13481325c03cffc693b49a412f0be5f
Pete Gadomski <pete.gadomski@gmail.com>
2017-09-25 16:05:52 +0000

    Add scanifc subcrate for wrapping c lib
    C++ so far is Too Hard.

---
+++ scanifc-sys/build.rs
@@ -0,0 +1,14 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=scanifc-mt");
+    let bindings = bindgen::builder()
+        .header("wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings");
+    let path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings.write_to_file(path.join("bindings.rs")).expect("Couldn't write bindings!");
+}
---
+++ scanifc-sys/wrapper.h
@@ -0,0 +1,1 @@
+#include <riegl/scanifc.h>
